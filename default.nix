c5a24b7a8be203d99d4d0591959a70921f6bd65c
Han Verstraete <welteki@pm.me>
2021-06-01 15:01:51 +0200

    Support legacy Nix
    - Use flake-compat (https://github.com/edolstra/flake-compat) to allow flake install in legacy nix
      see: https://nixos.wiki/wiki/Flakes

--- default.nix
+++ default.nix
@@ -1,36 +1,10 @@
-with (import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/80fcac0b311031657783b721c935d2d9348dffee.tar.gz") { });
-
-buildGoModule rec {
-  pname = "faas-cli";
-  version = "0.13.9";
-  commit = "2cec97955a254358de5443987bedf8ceee272cf8";
-
-  src = fetchFromGitHub {
-    rev = "${version}";
-    owner = "openfaas";
-    repo = "faas-cli";
-    sha256 = "102gsa9v4nvnvrvgm64hkv0ksnz8hhm5y6kqqkcvmij2x65y7ab3";
-  };
-
-  vendorSha256 = null;
-
-  subPackages = [ "." ];
-
-  CGO_ENABLED = 0;
-  buildFlagsArray = [
-    ''
-      -ldflags=
-      -s -w 
-      -X github.com/openfaas/faas-cli/version.GitCommit=${commit}
-      -X github.com/openfaas/faas-cli/version.Version=${version}
-      -X github.com/openfaas/faas-cli/commands.Platform=${builtins.currentSystem}
-    ''
-    "-a"
-  ];
-
-  meta = with lib; {
-    description = "Official CLI for OpenFaaS";
-    homepage = "https://github.com/openfaas/faas-cli";
-    license = licenses.mit;
-  };
-}
+(import
+  (
+    fetchTarball {
+      url = "https://github.com/edolstra/flake-compat/archive/99f1c2157fba4bfe6211a321fd0ee43199025dbf.tar.gz";
+      sha256 = "0x2jn3vrawwv9xp15674wjz9pixwjyj3j771izayl962zziivbx2";
+    }
+  )
+  {
+    src = ./.;
+  }).defaultNix
