e8d40b7fe37095ec94b3ca5914e4e8d752fb1e77
Han Verstraete <welteki@pm.me>
2021-06-02 22:47:46 +0200

    Support multiple systems

--- flake.nix
+++ flake.nix
@@ -14,34 +14,42 @@
       };
     };
 
-  outputs = { self, nixpkgs, flake-compat, faas-cli-src }: {
-    defaultPackage.x86_64-linux =
-      with import nixpkgs
-        {
-          system = "x86_64-linux";
-        };
-      buildGoModule rec {
-        pname = "faas-cli";
-        version = "0.13.9";
-        commit = "2cec97955a254358de5443987bedf8ceee272cf8";
+  outputs = { self, nixpkgs, flake-compat, faas-cli-src }:
+    let
+      supportedSystems = [ "x86_64-linux" "i686-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" "armv7l-linux" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+    in
+    {
+      overlay = final: prev: {
+        faas-cli = with final;
+          buildGoModule rec {
+            pname = "faas-cli";
+            version = "0.13.9";
+            commit = "2cec97955a254358de5443987bedf8ceee272cf8";
 
-        src = "${faas-cli-src}";
+            src = "${faas-cli-src}";
 
-        vendorSha256 = null;
+            vendorSha256 = null;
 
-        subPackages = [ "." ];
+            subPackages = [ "." ];
 
-        CGO_ENABLED = 0;
-        buildFlagsArray = [
-          ''
-            -ldflags=
-            -s -w 
-            -X github.com/openfaas/faas-cli/version.GitCommit=${commit}
-            -X github.com/openfaas/faas-cli/version.Version=${version}
-            -X github.com/openfaas/faas-cli/commands.Platform=${system}
-          ''
-          "-a"
-        ];
+            CGO_ENABLED = 0;
+            buildFlagsArray = [
+              ''
+                -ldflags=
+                -s -w 
+                -X github.com/openfaas/faas-cli/version.GitCommit=${commit}
+                -X github.com/openfaas/faas-cli/version.Version=${version}
+                -X github.com/openfaas/faas-cli/commands.Platform=${system}
+              ''
+              "-a"
+            ];
+          };
       };
-  };
+
+      defaultPackage = forAllSystems (system: (import nixpkgs {
+        inherit system;
+        overlays = [ self.overlay ];
+      }).faas-cli);
+    };
 }
