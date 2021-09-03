e082ea5f1e47e7ca51db5a83d563682e0a190a7c
Han Verstraete <welteki@pm.me>
2021-09-03 09:30:06 +0200

    Remove faas-cli from flake inputs
    Fetch source using fetchFromGitHub instead of passing it as flake input.

--- flake.nix
+++ flake.nix
@@ -1,20 +1,15 @@
 {
   description = "Official CLI for OpenFaaS ";
 
-  inputs =
-    {
-      nixpkgs.follows = "nix/nixpkgs";
-      flake-compat = {
-        url = "github:edolstra/flake-compat";
-        flake = false;
-      };
-      faas-cli-src = {
-        url = "https://github.com/openfaas/faas-cli/archive/refs/tags/0.13.13.tar.gz";
-        flake = false;
-      };
+  inputs = {
+    nixpkgs.follows = "nix/nixpkgs";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
     };
+  };
 
-  outputs = { self, nixpkgs, nix, flake-compat, faas-cli-src }:
+  outputs = { self, nixpkgs, nix, flake-compat }:
     let
       supportedSystems = [ "x86_64-linux" "i686-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" "armv7l-linux" ];
 
@@ -30,14 +25,22 @@
       forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
     in
     {
-      overlay = final: prev: {
-        faas-cli = with final;
-          buildGoModule rec {
+      overlay = final: prev:
+        let
+          inherit (final) buildGoModule fetchFromGitHub;
+        in
+        {
+          faas-cli = buildGoModule rec {
             pname = "faas-cli";
             version = "0.13.13";
-            commit = "72816d486cf76c3089b915dfb0b66b85cf096634";
+            rev = "72816d486cf76c3089b915dfb0b66b85cf096634";
 
-            src = "${faas-cli-src}";
+            src = fetchFromGitHub {
+              owner = "openfaas";
+              repo = "faas-cli";
+              rev = "${rev}";
+              sha256 = "0mmrakyy2qmkldld7pxf5bx6whdadq2r52b68f9p9z7yqrdimix8";
+            };
 
             vendorSha256 = null;
 
@@ -48,14 +51,14 @@
               ''
                 -ldflags=
                 -s -w 
-                -X github.com/openfaas/faas-cli/version.GitCommit=${commit}
+                -X github.com/openfaas/faas-cli/version.GitCommit=${rev}
                 -X github.com/openfaas/faas-cli/version.Version=${version}
-                -X github.com/openfaas/faas-cli/commands.Platform=${faasDefaultPlatforms.${system}}
+                -X github.com/openfaas/faas-cli/commands.Platform=${faasDefaultPlatforms.${final.system}}
               ''
               "-a"
             ];
           };
-      };
+        };
 
       defaultPackage = forAllSystems (system: (import nixpkgs {
         inherit system;
